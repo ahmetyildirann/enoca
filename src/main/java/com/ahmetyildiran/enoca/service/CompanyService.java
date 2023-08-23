eb5eb5bff7e89906184d5a44f0b7df9734205fe2
ahmetyildirann <56588376+ahmetyildirann@users.noreply.github.com>
2023-08-23 14:43:40 +0300

    add log

--- src/main/java/com/ahmetyildiran/enoca/service/CompanyService.java
+++ src/main/java/com/ahmetyildiran/enoca/service/CompanyService.java
@@ -6,17 +6,20 @@ import com.ahmetyildiran.enoca.mapper.CompanyMapper;
 import com.ahmetyildiran.enoca.model.Company;
 import com.ahmetyildiran.enoca.service.entityService.CompanyEntityService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class CompanyService {
 
   private final CompanyEntityService companyEntityService;
 
     public CompanyDto save(CompanySaveRequest request) {
+        log.info("CompanyEntityService::save|CompanySaveRequest"+request);
         Company company = CompanyMapper.convertRequestToCompany(request);
         company = companyEntityService.save(company);
 
@@ -25,8 +28,8 @@ public class CompanyService {
     }
 
     public List<CompanyDto> findAll() {
-
         List<Company> companyList = companyEntityService.findAll();
+        log.info("CompanyEntityService::findAll|companyList" + companyList);
         if (companyList.isEmpty()){
             throw new IllegalStateException("Company list is empty");
         }
@@ -36,7 +39,9 @@ public class CompanyService {
 
 
     public CompanyDto findById(Long id) {
+        log.info("CompanyEntityService::findById|id" + id);
         Company company = companyEntityService.findByIdWithControl(id);
+        log.info("CompanyEntityService::findById|company" + company);
         if (company.getId() == null){
             throw new IllegalStateException("Company not found with id " + id);
         }
@@ -46,6 +51,7 @@ public class CompanyService {
 
 
     public void delete(Long id) {
+        log.info("CompanyEntityService::delete|id" + id);
         boolean exist = companyEntityService.isExist(id);
         if (!exist){
             throw new IllegalStateException("Company not found with id " + id);
@@ -58,6 +64,7 @@ public class CompanyService {
 
 
     public CompanyDto update(CompanyDto companyDto) {
+        log.info("CompanyEntityService::update|companyDto"+companyDto);
         boolean exist = companyEntityService.isExist(companyDto.getId());
         if (!exist){
             throw new IllegalStateException("Company not found with id " + companyDto.getId());
--- src/main/java/com/ahmetyildiran/enoca/service/EmployeeService.java
+++ src/main/java/com/ahmetyildiran/enoca/service/EmployeeService.java
@@ -6,16 +6,19 @@ import com.ahmetyildiran.enoca.mapper.EmployeeMapper;
 import com.ahmetyildiran.enoca.model.Employee;
 import com.ahmetyildiran.enoca.service.entityService.EmployeeEntityService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class EmployeeService {
 
     private final EmployeeEntityService employeeEntityService;
     public EmployeeDto save(EmployeeSaveRequest employeeSaveRequest) {
+        log.info("EmployeeEntityService::save|employeeSaveRequest"+employeeSaveRequest);
         Employee employee = EmployeeMapper.convertRequestToEmployee(employeeSaveRequest);
         employee = employeeEntityService.save(employee);
 
@@ -26,6 +29,7 @@ public class EmployeeService {
 
     public List<EmployeeDto> findAll() {
         List<Employee> employeeList = employeeEntityService.findAll();
+        log.info("EmployeeEntityService::findAll|employeeList"+employeeList);
         if (employeeList.isEmpty()) {
             throw new IllegalStateException("Employee list is empty");
         }
@@ -34,6 +38,7 @@ public class EmployeeService {
     }
 
     public EmployeeDto findById(Long id) {
+        log.info("EmployeeEntityService::findById|id"+id);
         Employee employee = employeeEntityService.findByIdWithControl(id);
         EmployeeDto employeeDto = EmployeeMapper.convertEmployeeDto(employee);
         return employeeDto;
@@ -41,6 +46,7 @@ public class EmployeeService {
 
 
     public void delete(Long id) {
+        log.info("EmployeeEntityService::delete|id"+id);
         boolean exist = employeeEntityService.isExist(id);
         if (!exist) {
             throw new IllegalStateException("Employee not found with id " + id);
@@ -50,6 +56,7 @@ public class EmployeeService {
     }
 
     public EmployeeDto update(EmployeeDto employeeDto) {
+        log.info("EmployeeEntityService::update|employeeDto"+employeeDto);
         boolean exist = employeeEntityService.isExist(employeeDto.getId());
         if (!exist) {
             throw new IllegalStateException("Employee not found");
